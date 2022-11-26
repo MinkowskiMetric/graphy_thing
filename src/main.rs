ae3d41eb5c4e51bff6d4914a171a9bd3f0bd0b43
Stewart Tootill <stewart.tootill@live.co.uk>
2022-11-26 12:18:18 +0000

    tidy get output

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use std::fmt;
 use console_engine::KeyCode;
 
 #[derive(Debug)]
-struct GameCell {
-
+struct GameCell { 
+    op: u32,
 }
 
-impl fmt::Display for GameCell {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        write!(f, "{}", 0)
+impl GameCell {
+    pub fn new() -> Self {
+        GameCell { op: 0 }
+    }
+}
+
+impl GameCell {
+    fn get(&self) -> &str {
+        if self.op == 0 {
+            "A"
+        } else {
+            "B"
+        }
     }
 }
 
@@ -27,7 +36,7 @@ impl Game {
     pub fn from_dimensions(dimensions: (usize, usize)) -> Self {
         let mut data = Vec::with_capacity(dimensions.0 * dimensions.1);
         for _ in 0..(dimensions.0 * dimensions.1) {
-            data.push(GameCell {});
+            data.push(GameCell::new());
         }
         
         let engine = console_engine::ConsoleEngine::init(20, 20, 3).unwrap();
@@ -73,7 +82,8 @@ impl Game {
         for col in 0..mul_col {
             self.engine.print(x, y + 1 + col, "|");
             for r in 0..mul_row {
-                self.engine.print(x + 1 + col, y + 1 + r, "0");
+                let scan = self.get((col as usize, r as usize)).get().to_owned();
+                self.engine.print(x + 1 + col, y + 1 + r, scan.as_str());
             }
             self.engine.print(x + 1 + mul_col, y + 1 + col, "|");
         }
@@ -83,34 +93,6 @@ impl Game {
             self.engine.print(x + 1 + col, y + 1 + mul_row, "-");
         }
         self.engine.print(x + 1 + mul_col, y + 1 + mul_row, "+");
-    }
-}
-
-impl fmt::Display for Game {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        let col = self.dimensions.0;
-
-        write!(f, "+")?;
-        for _ in 0..col {
-            write!(f, "-")?;
-        }
-        writeln!(f, "+")?;
-
-        for col in 0..col {
-            write!(f, "|")?;
-            for row in 0..self.dimensions.1 {
-                self.get((col, row)).fmt(f)?;
-            }
-            writeln!(f, "|")?;
-        }
-
-        write!(f, "+")?;
-        for _ in 0..col {
-            write!(f, "-")?;
-        }
-        write!(f, "+")?;
-
-        Ok(())
     }
 }
 
