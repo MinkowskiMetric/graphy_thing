af303f72e65382f472b0ea7abb4ee4cea8409665
Stewart Tootill <stewart.tootill@live.co.uk>
2022-11-26 11:03:40 +0000

    a better look

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use console_engine::KeyCode;
 
 #[derive(Debug)]
 struct GameCell {
@@ -11,10 +12,10 @@ impl fmt::Display for GameCell {
     }
 }
 
-#[derive(Debug)]
 struct Game {
     dimensions: (usize, usize),
     data: Box<[GameCell]>,
+    engine: console_engine::ConsoleEngine,
 }
 
 impl Game {
@@ -28,8 +29,9 @@ impl Game {
         for _ in 0..(dimensions.0 * dimensions.1) {
             data.push(GameCell {});
         }
-        println!{"size: {}", dimensions.0 * dimensions.1};
-        Game { dimensions, data: data.into_boxed_slice() }
+        
+        let engine = console_engine::ConsoleEngine::init(20, 20, 3).unwrap();
+        Game { dimensions, data: data.into_boxed_slice(), engine }
     }
 
     pub fn get(&self, pt: (usize, usize)) -> &GameCell {
@@ -39,6 +41,48 @@ impl Game {
     #[allow(dead_code)]
     pub fn get_mut(&mut self, pt: (usize, usize)) -> &mut GameCell {
         &mut self.data[(self.dimensions.0 * pt.0) + pt.1]
+    }
+
+    fn single_input(&mut self) -> Option<()> {
+        self.engine.wait_frame();
+        self.engine.clear_screen();
+
+        if self.engine.is_key_pressed(KeyCode::Char('q')) {
+            None
+        } else {
+            self.print(0, 0);
+            self.engine.draw();
+            Some(())
+        }
+    }
+
+    pub fn play(mut self) {
+        while self.single_input() == Some(()) {
+        }
+    }
+
+    pub fn print(&mut self, x: i32, y: i32) {
+        let (mul_col, mul_row) = (self.dimensions.0 as i32, self.dimensions.1 as i32);
+
+        self.engine.print(x, y, "+");
+        for col in 0..mul_col {
+            self.engine.print(x + 1 + col, y, "-");
+        }
+        self.engine.print(x + 1 + mul_col, y, "+");
+
+        for col in 0..mul_col {
+            self.engine.print(x, y + 1 + col, "|");
+            for r in 0..mul_row {
+                self.engine.print(x + 1 + col, y + 1 + r, "0");
+            }
+            self.engine.print(x + 1 + mul_col, y + 1 + col, "|");
+        }
+        
+        self.engine.print(x, y + 1 + mul_row, "+");
+        for col in 0..mul_col {
+            self.engine.print(x + 1 + col, y + 1 + mul_row, "-");
+        }
+        self.engine.print(x + 1 + mul_col, y + 1 + mul_row, "+");
     }
 }
 
@@ -72,8 +116,5 @@ impl fmt::Display for Game {
 
 fn main() {
     let dimensions = (15, 15);
-    let game = Game::from_dimensions(dimensions);
-
-    println!("Hello, world!");
-    println!("Box:\n{}", game);
+    Game::from_dimensions(dimensions).play();
 }
