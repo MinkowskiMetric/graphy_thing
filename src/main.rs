a1584cbe8a013490b81381d52966a1e13e061db6
Stewart Tootill <stewart.tootill@live.co.uk>
2022-11-30 11:31:42 +0000

    Less keyboard

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,29 @@
 use console_engine::KeyCode;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum GameSet {
+enum GameCommand {
+    #[allow(dead_code)]
     On,
-    Off,
 }
 
-impl GameSet {
-    fn as_scan(&self) -> KeyCode {
-        match &self {
-            GameSet::On => KeyCode::Char('A'),
-            GameSet::Off => KeyCode::Char('B'),
-        }
-    }
+struct PlayBookCell {
 
-    fn as_str(&self) -> &str {
-        match self {
-            GameSet::On => "A",
-            GameSet::Off => "B",
-        }
-    }
 }
 
-#[derive(Debug)]
-struct GameCell { 
-    op: GameSet,
-    #[allow(dead_code)]
-    focused: bool,
-}
-
-impl GameCell {
+impl PlayBookCell {
     pub fn new() -> Self {
-        GameCell { op: GameSet::On, focused: false }
-    }
-
-    #[allow(dead_code)]
-    fn get(&self) -> GameSet {
-        self.op
-    }
-
-    #[allow(dead_code)]
-    fn set(&mut self, op: GameSet) {
-        self.op = op;
-    }
-
-    #[allow(dead_code)]
-    fn get_focus(&self) -> bool {
-        self.focused
-    }
-
-    #[allow(dead_code)]
-    fn set_focus(&mut self, set_focus: bool) {
-        self.focused = set_focus;
+        PlayBookCell { }
     }
 }
 
-#[derive(Debug)]
-struct Input {
-    key: GameSet,
-    location: Option<GameCell>,
-}
-
-impl Input {
-    pub fn setup(key: GameSet) -> Self {
-        Self {
-            key,
-            location: None,
-        }
-    }
-
-    pub fn on_key(&mut self, location: Option<GameCell>) {
-        self.location = location;
-    }
-}
-
-struct Game {
+struct PlayBook {
     dimensions: (usize, usize),
-    data: Box<[GameCell]>,
+    #[allow(dead_code)]
+    data: Box<[PlayBookCell]>,
     engine: console_engine::ConsoleEngine,
-    inputs: Box<[Input]>,
 }
 
-impl Game {
+impl PlayBook {
     #[allow(dead_code)]
     pub fn from_sizes(width: usize, height: usize) -> Self {
         Self::from_dimensions((width, height))
@@ -90,25 +32,11 @@ impl Game {
     pub fn from_dimensions(dimensions: (usize, usize)) -> Self {
         let mut data = Vec::with_capacity(dimensions.0 * dimensions.1);
         for _ in 0..(dimensions.0 * dimensions.1) {
-            data.push(GameCell::new());
+            data.push(PlayBookCell::new());
         }
-        
-        let inputs = vec![
-            Input::setup(GameSet::On),
-            Input::setup(GameSet::Off),
-        ];
 
         let engine = console_engine::ConsoleEngine::init(20, 20, 3).unwrap();
-        Game { dimensions, data: data.into_boxed_slice(), engine, inputs: inputs.into_boxed_slice(), }
-    }
-
-    pub fn get(&self, pt: (usize, usize)) -> &GameCell {
-        &self.data[(self.dimensions.0 * pt.0) + pt.1]
-    }
-
-    #[allow(dead_code)]
-    pub fn get_mut(&mut self, pt: (usize, usize)) -> &mut GameCell {
-        &mut self.data[(self.dimensions.0 * pt.0) + pt.1]
+        PlayBook { dimensions, data: data.into_boxed_slice(), engine, }
     }
 
     fn single_input(&mut self) -> Option<()> {
@@ -118,14 +46,6 @@ impl Game {
         if self.engine.is_key_pressed(KeyCode::Char('q')) {
             None
         } else {
-            for input in self.inputs.iter_mut() {
-                if self.engine.is_key_pressed(input.key.as_scan()) {
-                    input.on_key(Some(GameCell { op: input.key, focused: false }));
-                } else {
-                    input.on_key(None);
-                }
-            }
-
             self.print(0, 0);
             self.engine.draw();
             Some(())
@@ -148,9 +68,8 @@ impl Game {
 
         for col in 0..mul_col {
             self.engine.print(x, y + 1 + col, "|");
-            for r in 0..mul_row {
-                let scan = self.get((col as usize, r as usize));
-                self.engine.print(x + 1 + col, y + 1 + r, scan.op.as_str().to_owned().as_str());
+            for _row in 0..mul_row {
+                // TODO self.engine.print(x + 1 + col, y + 1 + r, self.cell_layout((col as usize, row as usize)).as_scan());
             }
             self.engine.print(x + 1 + mul_col, y + 1 + col, "|");
         }
@@ -165,5 +84,5 @@ impl Game {
 
 fn main() {
     let dimensions = (15, 15);
-    Game::from_dimensions(dimensions).play();
+    PlayBook::from_dimensions(dimensions).play();
 }
