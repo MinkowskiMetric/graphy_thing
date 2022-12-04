b0db0f9a26bfd36a05373d53230a88ce65bee5b3
Stewart Tootill <stewart.tootill@live.co.uk>
2022-12-04 12:44:42 +0000

    e Please enter the commit message for your changes. Lines starting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
-use console_engine::{KeyCode, events::Event};
-use std::fmt;
+use console_engine::{KeyCode, events::Event, crossterm::event::MouseEvent};
+use std::{cell::RefCell, fmt};
+
+struct ConsoleGraph {
+    e: RefCell<console_engine::ConsoleEngine>,
+}
+
+impl ConsoleGraph {
+    pub fn init(width: u32, height: u32, target_fps: u32) -> Result<Self, std::io::Error> {
+        match console_engine::ConsoleEngine::init(width, height, target_fps) {
+            Ok(e) => Ok(Self { e: RefCell::new(e) }),
+            Err(e) => Err(e),
+        }
+    }
+
+    pub fn poll(&self) -> Event {
+        self.e.borrow_mut().poll()
+    }
+
+    pub fn clear_screen(&self) {
+        self.e.borrow_mut().clear_screen();
+    }
+
+    pub fn draw(&self) {
+       self.e.borrow_mut().draw();
+    }
+
+    pub fn print(&self, x: i32, y: i32, string: &str) {
+        self.e.borrow_mut().print(x, y, string)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum SingleCommand {
+    Quit,
+}
+
+impl fmt::Display for SingleCommand {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SingleCommand::Quit => f.write_str("Quit"),
+        }
+    }    
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum GameCommand {
@@ -7,6 +49,7 @@ enum GameCommand {
     On,
     Off,
 }
+
 
 impl fmt::Display for GameCommand {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -16,6 +59,7 @@ impl fmt::Display for GameCommand {
         }
     }
 }
+
 
 struct PlayBookCell {
     #[allow(dead_code)]
@@ -39,7 +83,7 @@ impl PlayBookCell {
 struct PlayBook {
     dimensions: (usize, usize),
     data: Box<[PlayBookCell]>,
-    engine: console_engine::ConsoleEngine,
+    engine: ConsoleGraph,
 }
 
 impl PlayBook {
@@ -56,68 +100,79 @@ impl PlayBook {
             }
         }
 
-        let engine = console_engine::ConsoleEngine::init(20, 20, 3).unwrap();
-        PlayBook { dimensions, data: data.into_boxed_slice(), engine, }
+        PlayBook { dimensions, data: data.into_boxed_slice(), engine: ConsoleGraph::init(20, 25, 3).unwrap(), }
     }
 
-    fn single_input(&mut self) -> Option<()> {
+    fn single_input(&self) -> Option<SingleCommand> {
         match self.engine.poll() {
             Event::Frame => {
                 self.engine.clear_screen();
                 self.print(0, 0);
                 self.engine.draw();
-                Some(())
+                None
             },
 
             Event::Key(key) => {
-                if key.code == KeyCode::Char('q') {
-                    None
-                } else {
-                    Some(())
+                match key.code {
+                    KeyCode::Char('q') | KeyCode::Char('Q') => Some(SingleCommand::Quit),
+                    _ => None,
                 }
             },
 
-            Event::Mouse(_mouse) => {
-                Some(())
+            Event::Mouse(mouse) => {
+                let pos = (mouse.column as usize, mouse.row as usize);
+                if pos.0 < self.dimensions.0 && pos.1 < self.dimensions.1 {
+                    self.on_mouse(&self.data[(self.dimensions.0 * pos.1) * pos.0], pos, mouse)
+                } else {
+                    None
+                }
             },
 
-            Event::Resize(_width, _height) => {
-                Some(())
+            Event::Resize(_width, _heights) => {
+                None
             },
         }
     }
 
-    pub fn play(mut self) {
-        while self.single_input() == Some(()) {
+    pub fn play(self) {
+        while self.single_input().is_none() {
         }
     }
 
-    fn print_glyph(&mut self, x: i32, y: i32, shape: GameCommand) {
-        self.engine.print(x, y, &shape.to_owned().to_string());
+    fn on_mouse(&self, _play_room_cell: &PlayBookCell, _pos: (usize, usize), _mouse_event: MouseEvent) -> Option<SingleCommand> {
+        todo!()
     }
 
-    pub fn print(&mut self, x: i32, y: i32) {
+    fn print_code(&self, x: i32, y: i32, string: &str) {
+        self.engine.print(x, y, string);
+    }
+
+    fn print_glyph(&self, x: i32, y: i32, shape: GameCommand) {
+        self.print_code(x, y, shape.to_string().as_str());
+    }
+
+    pub fn print(&self, x: i32, y: i32) {
         let (mul_col, mul_row) = (self.dimensions.0 as i32, self.dimensions.1 as i32);
 
-        self.engine.print(x, y, "+");
+        self.print_code(x, y, "+");
         for col in 0..mul_col {
-            self.engine.print(x + 1 + col, y, "-");
+            self.print_code(x + 1 + col, y, "-");
         }
-        self.engine.print(x + 1 + mul_col, y, "+");
+        self.print_code(x + 1 + mul_col, y, "+");
 
         for col in 0..mul_col {
-            self.engine.print(x, y + 1 + col, "|");
+            self.print_code(x, y + 1 + col, "|");
             for row in 0..mul_row {
                 self.print_glyph(x + 1 + col, y + 1 + row, self.data[col as usize].get_visual());
             }
-            self.engine.print(x + 1 + mul_col, y + 1 + col, "|");
+            self.print_code(x + 1 + mul_col, y + 1 + col, "|");
         }
         
-        self.engine.print(x, y + 1 + mul_row, "+");
+        self.print_code(x, y + 1 + mul_row, "+");
         for col in 0..mul_col {
-            self.engine.print(x + 1 + col, y + 1 + mul_row, "-");
+            self.print_code(x + 1 + col, y + 1 + mul_row, "-");
         }
-        self.engine.print(x + 1 + mul_col, y + 1 + mul_row, "+");
+        self.print_code(x + 1 + mul_col, y + 1 + mul_row, "+");
     }
 }
 
